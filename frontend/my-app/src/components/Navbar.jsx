da2cba936e6b3758c015c4e29a8323c652edb0a8
DekunleJr <adekunlesa10@gmail.com>
2025-09-30 14:53:03 +0100

    logo update

--- frontend/my-app/src/components/Navbar.jsx
+++ frontend/my-app/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <div className="logo">Interd</div>
+      <div className="logo">Signature</div>
       <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
